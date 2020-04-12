d1a5bbf9c0300c1101f99aa81acc2c35d36b26e5
Karina Macancela <karymacancela@gmail.com>
2020-04-12 19:17:08 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{padding: 50}}>
+      <View>
+        <TextInput 
+          placeholder="Course Goal" 
+          style={{borderColor: 'black', borderWidth: 1, padding: 10}} />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
