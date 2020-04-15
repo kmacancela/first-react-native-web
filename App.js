55a45393976489c59b9b54de5da75f26abd76489
Karina Macancela <karymacancela@gmail.com>
2020-04-15 11:15:09 -0400

    adding styling

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
-  return (
-    <View style={{padding: 50}}>
-      <View>
-        <TextInput 
-          placeholder="Course Goal" 
-          style={{borderColor: 'black', borderWidth: 1, padding: 10}} />
-        <Button title="ADD" />
-      </View>
-      <View>
+  const [enteredGoal, setEnteredGoal] = useState('');
 
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD!" onPress={addGoalHandler}
+        />
       </View>
+      <View />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
 });
